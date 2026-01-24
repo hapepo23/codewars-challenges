9a83599555360ead79848a4f2fe62ad820fe5fcc
hapepo23 <github@hapepo.de>
2026-01-24 14:02:04 +0100

    update 2026-01-24 14:02 CET

--- rectangle_rotation_main.c
+++ rectangle_rotation_main.c
@@ -20,6 +20,11 @@ static void do_test(int a, int b, ll expected) {
 }
 
 int main(void) {
+  do_test(1, 1, 1);
+  do_test(2, 2, 5);
+  do_test(10, 10, 113);
+  do_test(20, 10, 217);
+  do_test(10, 20, 217);
   do_test(6, 4, 23);
   do_test(30, 2, 65);
   do_test(8, 6, 49);
